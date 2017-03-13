0113e303356e932d8dd4041b4a0e0779c94e63c6
Sondre Kongsgård <sondre.kongsgaard@gmail.com>
2017-03-13 10:28:35 +0100

    Add more console colors

--- utilities/typedef.go
+++ utilities/typedef.go
@@ -192,8 +192,11 @@ type ElevatorLight struct {
 
 // Console colors
 const (
+	ColorRed     = "\x1b[31;1m"
+	ColorGreen   = "\x1b[32;1m"
+	ColorBlue    = "\x1b[33;1m"
+	ColorYellow  = "\x1b[34;1m"
 	ColorWhite   = "\x1b[37;1m"
-	ColorGreen   = "\x1b[31;1m"
 	ColorNeutral = "\x1b[0m"
 )
 
